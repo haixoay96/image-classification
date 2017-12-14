e2f98da083fddd5750edd3329316629b73bc45b3
LinhND <haixoay96@gmail.com>
2017-12-14 15:07:25 +0700

    done

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ from keras import backend as K
 
 class LeNet:
     @staticmethod
-    def build(with, height, depth, classes):
+    def build(width, height, depth, classes):
         # initialize the model
         model = Sequential()
         inputShape = (height, width, depth)
@@ -18,7 +18,7 @@ class LeNet:
         # first set of CONV ==> RELU ==> POOL layers
         model.add(Conv2D(20, (50,50), padding='same', input_shape=inputShape))
         model.add(Activation('relu'))
-        model.add(MaxPooling2D(pool_size(2,2), strides=(2,2)))
+        model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
         # second set of CONV ==> RELU ==> POOL layers
         model.add(Conv2D(50, (5,5), padding='same'))
         model.add(Activation('relu'))
